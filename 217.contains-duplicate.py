c66ccfb69732f0c95b70a3958aef982642237362
ablades <audaris@gmail.com>
2020-08-22 15:21:40 -0500

    complete contains dup

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=217 lang=python
+#
+# [217] Contains Duplicate
+#
+# https://leetcode.com/problems/contains-duplicate/description/
+#
+# algorithms
+# Easy (55.97%)
+# Likes:    969
+# Dislikes: 758
+# Total Accepted:    602.9K
+# Total Submissions: 1.1M
+# Testcase Example:  '[1,2,3,1]'
+#
+# Given an array of integers, find if the array contains any duplicates.
+# 
+# Your function should return true if any value appears at least twice in the
+# array, and it should return false if every element is distinct.
+# 
+# Example 1:
+# 
+# 
+# Input: [1,2,3,1]
+# Output: true
+# 
+# Example 2:
+# 
+# 
+# Input: [1,2,3,4]
+# Output: false
+# 
+# Example 3:
+# 
+# 
+# Input: [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dup = set()
+
+        for num in nums:
+
+            if num in dup:
+                return True
+            else:
+                dup.add(num)
+                
+        return False
+
+
+        
+# @lc code=end
